daadf15d1062c8ff34165bfff79662bfc4bfc45a
kunickyd <danecek37@gmail.com>
2024-10-29 12:57:39 +0100

    dev: add test app

---
+++ testapp/models.py
@@ -0,0 +1,15 @@
+"""
+Create or customize your page models here.
+"""
+from coderedcms.models import CoderedWebPage
+
+
+class WebPage(CoderedWebPage):
+    """
+    General use page with featureful streamfield and SEO attributes.
+    """
+
+    class Meta:
+        verbose_name = "Web Page"
+
+    template = "coderedcms/pages/web_page.html"
---
+++ testapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "x&%ut=9of!ajna^@9rv91b*8u(845o+t=7b37sdq7f%&aqc(6d"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
